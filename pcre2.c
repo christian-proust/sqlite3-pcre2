fe9f0fe90d1d4eb9e472b722a95a03ce73ec6199
Christian Proust <christian.proust@gresille.org>
2022-04-11 09:43:21 +0200

    Implement REGEXP_SUBSTR

--- pcre2.c
+++ pcre2.c
@@ -160,6 +160,7 @@ pcre2_code* pcre2_compile_from_sqlite_cache(
                 ctx, error_code,
                 "Cannot compile REGEXP pattern %Q at offset %lu",
                 pattern_str, error_position
+                // FIXME Cannot display pattern in case of NUL character
             );
             return NULL;
         }
@@ -241,7 +242,7 @@ void regexp(sqlite3_context *ctx, int argc, sqlite3_value **argv) {
           sqlite3_result_int(ctx, 0);
         } else { // (rc < 0 and the code is not one of the above)
             error_pcre2sqlite(ctx, rc);
-            return;
+            return;  // FIXME memory leak
         }
         pcre2_match_data_free(match_data);
         return;
@@ -249,6 +250,76 @@ void regexp(sqlite3_context *ctx, int argc, sqlite3_value **argv) {
 }
 
 
+static void regexp_substr(
+        sqlite3_context *ctx,
+        int argc,
+        sqlite3_value **argv) {
+    const char *pattern_str, *subject_str;
+    int pattern_len, subject_len;
+    pcre2_code *pattern_code;
+
+    assert(argc == 2);      // TODO check argc == 1 and argc = 3
+    /* check NULL parameter */
+    for(int idx = 0; idx < 2; idx++) {
+      if(sqlite3_value_type(argv[idx]) == SQLITE_NULL) {
+        return;
+      }
+    }
+
+    /* extract parameter values */
+    subject_len = sqlite3_value_bytes(argv[0]);
+    subject_str = (const char *) sqlite3_value_text(argv[0]);
+    if (!subject_str) {
+        sqlite3_result_error(ctx, "no subject", -1);
+        return;
+    }
+
+    pattern_str = (const char *) sqlite3_value_text(argv[1]);
+    if (!pattern_str) {
+        sqlite3_result_error(ctx, "no pattern", -1);
+        return;
+    }
+    pattern_len = sqlite3_value_bytes(argv[1]);
+
+    pattern_code = pcre2_compile_from_sqlite_cache(
+        ctx, pattern_str, pattern_len
+    );
+    if(pattern_code == NULL) {
+        return;
+    }
+
+    {
+        int rc;
+        pcre2_match_data *match_data;
+
+        match_data = pcre2_match_data_create_from_pattern(pattern_code, NULL);
+        rc = pcre2_match(
+          pattern_code,         /* the compiled pattern */
+          subject_str,          /* the subject string */
+          subject_len,          /* the length of the subject */
+          0,                    /* start at offset 0 in the subject */
+          0,                    /* default options */
+          match_data,           /* block for storing the result */
+          NULL);                /* use default match context */
+
+        if(rc >= 0) {
+            size_t result_len;
+            unsigned char * result_str;
+            rc = pcre2_substring_get_bynumber(match_data, 0, &result_str, &result_len);
+            if(rc == 0) {
+                sqlite3_result_text(ctx, result_str, result_len, (void (*)(void *))pcre2_substring_free);
+            } else {
+                error_pcre2sqlite(ctx, rc);
+            }
+        } else if(rc == PCRE2_ERROR_NOMATCH) {
+            sqlite3_result_text(ctx, "", 0, SQLITE_STATIC);
+        } else { // (rc < 0 and the code is not one of the above)
+            error_pcre2sqlite(ctx, rc);
+        }
+        pcre2_match_data_free(match_data);
+        return;
+    }
+}
 static void regexp_replace(
         sqlite3_context *ctx,
         int argc,
@@ -357,6 +428,7 @@ static void regexp_replace(
     }
 }
 
+
 int sqlite3_extension_init(sqlite3 *db, char **err, const sqlite3_api_routines *api) {
     SQLITE_EXTENSION_INIT2(api)
     cache_entry *cache = calloc(CACHE_SIZE, sizeof(cache_entry));
@@ -365,6 +437,7 @@ int sqlite3_extension_init(sqlite3 *db, char **err, const sqlite3_api_routines *
         return 1;
     }
     sqlite3_create_function(db, "REGEXP", 2, SQLITE_UTF8, cache, regexp, NULL, NULL);
+    sqlite3_create_function(db, "REGEXP_SUBSTR", 2, SQLITE_UTF8, cache, regexp_substr, NULL, NULL);
     sqlite3_create_function(db, "REGEXP_REPLACE", 3, SQLITE_UTF8, cache, regexp_replace, NULL, NULL);
     return 0;
 }
